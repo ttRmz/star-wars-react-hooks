8297ed06204cf71355752b2bee903e357ce7cec9
Tristan RAMIREZ <tristan.rami@gmail.com>
2018-12-23 13:31:40 +0100

    fetch people

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import fetchPeople from './utils/fetchPeople';
 
 import { Header, AppWrapper } from './components';
 
 const App = () => {
   const [name, setName] = useState('');
+  const [people, setPeople] = useState(null);
+  useEffect(
+    () => {
+      fetchPeople(name).then(data => setPeople(data));
+    },
+    [name]
+  );
   return (
     <AppWrapper>
       <Header />
@@ -14,6 +22,10 @@ const App = () => {
         onChange={evt => setName(evt.target.value)}
         value={name}
       />
+      {people &&
+        people.results.map((result, index) => (
+          <div key={index}>{result.name}</div>
+        ))}
     </AppWrapper>
   );
 };
